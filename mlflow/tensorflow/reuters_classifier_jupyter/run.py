f846c5f6b2b8ee6b8ba33d53f9a4595a78c1320b
Nestsiarenia, Ihar <Ihar_Nestsiarenia@epam.com>
2020-01-17 16:50:32 +0300

    Removed params

--- mlflow/tensorflow/reuters_classifier_jupyter/run.py
+++ mlflow/tensorflow/reuters_classifier_jupyter/run.py
@@ -2,8 +2,8 @@ import papermill as pm
 
 pm.execute_notebook(
    'reuters_clf.ipynb',
-   'out_reuters_clf.ipynb',
-   parameters=dict(alpha=0.6, ratio=0.1)
+   'out_reuters_clf.ipynb'
+   #parameters=dict(alpha=0.6, ratio=0.1)
 )
 
 with mlflow.start_run():
