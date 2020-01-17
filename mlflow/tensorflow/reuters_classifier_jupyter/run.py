7a93ff7275409f2599f294972efb3bb6790b71de
Nestsiarenia, Ihar <Ihar_Nestsiarenia@epam.com>
2020-01-17 18:52:03 +0300

    Upd

--- mlflow/tensorflow/reuters_classifier_jupyter/run.py
+++ mlflow/tensorflow/reuters_classifier_jupyter/run.py
@@ -1,13 +1,10 @@
 import papermill as pm
 import mlflow
 
-pm.execute_notebook(
-   'reuters_clf.ipynb',
-   'out_reuters_clf.ipynb'
-)
+pm.execute_notebook('reuters_clf.ipynb', 'out_reuters_clf.ipynb')
 
 # #parameters=dict(alpha=0.6, ratio=0.1)
 
 with mlflow.start_run():
-   mlflow.log_metric("testkey", "testvalue")
-   mlflow.log_artifact("out_reuters_clf.ipynb")
+    mlflow.log_metric("testkey", 42)
+    mlflow.log_artifact("out_reuters_clf.ipynb")
