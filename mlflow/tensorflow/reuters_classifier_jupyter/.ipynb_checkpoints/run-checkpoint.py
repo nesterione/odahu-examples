9f8aa7258aec5b0efbdf3e0f00b9b7e6b6c7d270
Nestsiarenia, Ihar <Ihar_Nestsiarenia@epam.com>
2020-01-17 16:35:40 +0300

    Added jupyter demo

---
+++ mlflow/tensorflow/reuters_classifier_jupyter/.ipynb_checkpoints/run-checkpoint.py
@@ -0,0 +1,10 @@
+import papermill as pm
+
+pm.execute_notebook(
+   'reuters_clf.ipynb',
+   'out_reuters_clf.ipynb',
+   parameters=dict(alpha=0.6, ratio=0.1)
+)
+
+with mlflow.start_run():
+    mlflow.log_artifacts("out_reuters_clf.ipynb")
